47cd95e71cc225c8569138e0632063123312066d
valeria-cardenas <valeria.cardenas17@outlook.com>
2026-08-20 04:46:26 -0500

    cambio railway

--- src/main/java/com/appzoi/appzoi/AppzoiApplication.java
+++ src/main/java/com/appzoi/appzoi/AppzoiApplication.java
@@ -2,12 +2,17 @@ package com.appzoi.appzoi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.appzoi.appzoi")
+
 public class AppzoiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AppzoiApplication.class, args);
-    }
 
+
+
+    }
 }
